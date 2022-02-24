9af03ad76e21b4ac210c9234717cfac2e617b5e2
uf-code <ugur.firat.developer@gmail.com>
2022-02-24 11:11:11 +0300

    trying to handle props

--- frontend-phone/src/components/EditCustomer.svelte
+++ frontend-phone/src/components/EditCustomer.svelte
@@ -9,31 +9,81 @@
         ListInput,
         Button,
     } from 'framework7-svelte'
+    import { beforeUpdate } from 'svelte'
     import axios from '../js/axios.js'
+    import { onMount } from 'svelte'
     import { store_customers } from '../js/customer_store.js'
     export let customer_id
-
+    let flag = false
     let first_name, last_name, email, birthdate
+    let test_id = 0
 
-    const current_customer = $store_customers.find(
-        (customer) => customer.id == customer_id
-    )
+    // beforeUpdate(() => {
+    //     console.log(customer_id)
+    //     console.log(customer_id)
+    //     console.log(customer_id)
+    //     console.log(customer_id)
+    //     console.log(customer_id)
+    // })
 
-    first_name = current_customer.first_name
-    last_name = current_customer.last_name
-    email = current_customer.email
-    birthdate = current_customer.birthdate
+    // // onMount(() => {
+    //     const hey = filter_same_id(customer_id)
+    //     console.log(hey)
+    //     // console.log('object')
+    //     // console.log(customer_id)
 
-    const handleEdit = () => {
-        console.log('hey')
+    //     // console.log($store_customers)
+    //     // console.log('object')
+    //     // let current_customer = filter_same_id(customer_id)
+
+    //     // console.log(current_customer)
+
+    //     // first_name = current_customer.first_name
+    //     // last_name = current_customer.last_name
+    //     // email = current_customer.email
+    //     // birthdate = current_customer.birthdate
+
+    //     // console.log(first_name, last_name, email, birthdate)
+    // })
+
+    // let find_customer = (customer_id_) => {
+    //     $store_customers.find((customer) => customer.id === customer_id_)
+    // }
+
+    let filter_same_id = (customer_id_) =>
+        $store_customers.filter((customer) => customer.id == customer_id_)[0]
+
+    const openEdit = () => {
+        console.log(customer_id)
+        console.log(customer_id)
+        // test_id = id_
+    }
+
+    const handleEdit = (t_id) => {
+        console.log(t_id)
+        // axios
+        //     .put(`/updateCustomer/${customer_id}`, {
+        //         first_name: first_name,
+        //         last_name: last_name,
+        //         email: email,
+        //         birthdate: birthdate,
+        //     })
+        //     .then((res) => {
+        //         store_customers.set($store_customers)
+        //     })
+        //     .catch((err) => {
+        //         console.error(err)
+        //     })
     }
 </script>
 
-<Button fill round popupOpen=".demo-popup-swipe">Edit</Button>
+<Button fill round on:click={openEdit} popupOpen=".demo-popup-swipe"
+    >Edit</Button
+>
 
 <Popup class="demo-popup-swipe" swipeToClose>
     <Page>
-        <Navbar title="Add New Customer">
+        <Navbar title="Edit Customer Hey">
             <NavRight>
                 <Link popupClose>Close</Link>
             </NavRight>
@@ -91,7 +141,6 @@
                 type="datepicker"
                 placeholder="Select date"
                 readonly
-                value={birthdate}
                 calendarParams={{
                     openIn: 'customModal',
                     header: true,
@@ -108,7 +157,7 @@
                 round
                 small
                 popupClose=".demo-popup-swipe"
-                on:click={() => handleEdit()}>Add Customer</Button
+                on:click={handleEdit(test_id)}>Edit Customer</Button
             >
         </List>
 
